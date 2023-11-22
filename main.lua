96105dea1c1cb48484e941494489ab5435945896
FreekzOfficial <151761680+FreekzOfficial@users.noreply.github.com>
2023-11-22 10:17:40 -0500

    Main Hub

---
+++ main.lua
@@ -0,0 +1,8 @@
+local SelfModules = {
+  Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/FreekzOfficial/FreekzHub/main/functions.lua"), "Error loading functions")()
+}
+
+local LocalPlayer = game:GetService
+local OrionLib = _GHTTP('https://raw.githubusercontent.com/shlexware/Orion/main/source', nil, true)()
+
+local Window = OrionLib:MakeWindow({Name = "FreekzHub", IntroEnabled = true, IntroText = "Welcome to FreekzHub ", HidePremium = false})
