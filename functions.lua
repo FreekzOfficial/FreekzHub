6e275f98bc895db235d6255d090509c5e8323a76
FreekzOfficial <151761680+FreekzOfficial@users.noreply.github.com>
2023-11-22 10:42:37 -0500

    Utility Functions

--- functions.lua
+++ functions.lua
@@ -8,12 +8,19 @@ Functions._SRV = function(ServiceName: string)
   end
   return Service;
 end
+
 Functions._GHTTP = function(URL: string, chunkname: string, toLoadString: boolean)
   if (type(chunkname) ~= "string" or chunkname == nil) then chunkname = "contents" end
   if (toLoadString == true) then
    return loadstring(game:HttpGet(URL, chunkname)) ;
   end
   return game:HttpGet(URL, chunkname);
+end
+
+for FName, FFunc in next, Functions do
+    if typeof(FFunc) == "function" then
+        getgenv()[FName] = FFunc
+    end
 end
 
 return Functions
