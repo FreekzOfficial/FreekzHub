783a000d4e9d805f2c4f0e5a4bc55af54deb63a6
FreekzOfficial <151761680+FreekzOfficial@users.noreply.github.com>
2023-11-22 11:34:07 -0500

    Main Hub V1

--- main.lua
+++ main.lua
@@ -1,10 +1,13 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FreekzOfficial/FreekzHub/main/functions.lua"))()
 local LocalPlayer = _SRV("Players").LocalPlayer
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local gid = game.GameId
+local gameIds = {
+	["66654135"] = "MurderMystery2"
+}
+local OrionLib = _GHTTP('https://raw.githubusercontent.com/shlexware/Orion/main/source', 'contents', true)()
 local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-local Tab = Window:MakeTab({
-	Name = "Tab 1",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
+getgenv()["OrionWindow"] = Window
+if (table.find(gameIds, tostring(gid)) then
+	_GHTTP("https://raw.githubusercontent.com/FreekzOfficial/FreekzHub/main/games/" .. gameIds[tostring(gid)] .. ".lua", "contents", true)()
+end
 OrionLib:Init()
