f471bcac9059c917574d04f705c87af367a991af
FreekzOfficial <151761680+FreekzOfficial@users.noreply.github.com>
2023-11-22 10:22:05 -0500

    Main Hub V1

--- main.lua
+++ main.lua
@@ -6,4 +6,9 @@ local LocalPlayer = _SRV("Players").LocalPlayer
 local OrionLib = _GHTTP('https://raw.githubusercontent.com/shlexware/Orion/main/source', nil, true)()
 
 local Window = OrionLib:MakeWindow({Name = "FreekzHub", IntroEnabled = true, IntroText = "Welcome to FreekzHub ", HidePremium = false})
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
 print(LocalPlayer)
