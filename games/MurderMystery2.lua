8f04ab593e7c7fe647642e66e0892a0146eaeb40
FreekzOfficial <151761680+FreekzOfficial@users.noreply.github.com>
2023-11-22 11:40:50 -0500

    MM2

--- games/MurderMystery2.lua
+++ games/MurderMystery2.lua
@@ -1,3 +1,9 @@
 if (OrionWindow) then
   game.Players.LocalPlayer:Kick("Fr")
 end
+if (OrionWin) then
+  game.Players.LocalPlayer:Kick("Fr1")
+end
+if (_G.OrionW) then
+  game.Players.LocalPlayer:Kick("Fr3")
+end
