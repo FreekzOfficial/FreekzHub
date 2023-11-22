8598ac67225cb54b9dbb1a8b3179b627339f571b
FreekzOfficial <151761680+FreekzOfficial@users.noreply.github.com>
2023-11-22 11:03:08 -0500

    Main Hub V1

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FreekzOfficial/FreekzHub/main/functions.lua"))()
 
 local LocalPlayer = _SRV("Players").LocalPlayer
-local OrionLib = _GHTTP('https://raw.githubusercontent.com/shlexware/Orion/main/source', nil, true)()
+local OrionLib = loadstring(_GHTTP('https://raw.githubusercontent.com/shlexware/Orion/main/source', 'contents'))()
 
 local Window = OrionLib:MakeWindow({Name = "FreekzHub", IntroEnabled = true, IntroText = "Welcome to FreekzHub ", HidePremium = false})
 local Tab = Window:MakeTab({
